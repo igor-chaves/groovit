2b8cb8444ec1f087a3deb5cb89f7925d50e5e5db
Igor Chaves Donega <ichaves@live.com>
2025-06-28 00:07:27 +0900

    adds service section

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,63 @@ export default function Home() {
       <section className="container-lg">
         <h1>SECTION 2 - large</h1>
       </section>
+      {/* SERVICES SECTION */}
       <section className="container-md">
-        <h1>SECTION 3 - mid</h1>
+        <h2 className="text-center">Our Services</h2>
+        <div className="flex gap-16 justify-center items-center text-center">
+          <div className="flex flex-col gap-6">
+            <h3>システムエンジニアリングサービス (SES)</h3>
+            <img
+              src="https://images.unsplash.com/photo-1551434678-e076c223a692?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+            <ul className="text-sm text-left flex  flex-col  gap-2">
+              <li>
+                <span>必要に応じて迅速に技術支援を提供可能です。</span>
+              </li>
+              <li>
+                <span>繁忙期におけるチーム強化に最適です。</span>
+              </li>
+              <li>
+                <span>専門スキルを持つエンジニアを柔軟に活用できます。</span>
+              </li>
+              <li>
+                <span>業務遂行に特化した柔軟な契約形態に対応します。</span>
+              </li>
+              <li>
+                <span>クライアントチームと高い連携性を確保します。</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex gap-16 justify-center items-center text-center">
+            <div className="flex flex-col gap-6">
+              <h3>システムインテグレーター (SIER)</h3>
+              <img
+                src="https://images.unsplash.com/photo-1617529497471-9218633199c0?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+
+              <ul className="text-sm text-left flex flex-col gap-2">
+                <li>
+                  <span>プロジェクト全体の責任を一括で担います。</span>
+                </li>
+                <li>
+                  <span>要件定義から運用保守まで一貫した計画を実施します。</span>
+                </li>
+                <li>
+                  <span>お客様の業務に最適化されたシステムを構築します。</span>
+                </li>
+                <li>
+                  <span>品質・納期・範囲を一元管理します。</span>
+                </li>
+                <li>
+                  <span>多分野に対応可能な専門チームを編成します。</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
